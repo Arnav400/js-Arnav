c79b7b526a76fbe5be9cd3f726216f41b91f2558
Arnav004 <141306851+Arnav400@users.noreply.github.com>
2024-01-20 10:16:51 +0000

    string

---
+++ .devcontainer/01_basics/strings.js
@@ -0,0 +1,74 @@
+
+const name = "Arnav"
+const repoCount = 5
+//console.log(name + repoCount + " Value")
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+/*
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+*/
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+/*
+const gameName = new String('Arnav-is-the-best')
+console.log(gameName.trim())
+console.log(gameName.trim())
+*/
+/*
+const gameName = new String('        hitesh-hc-com         ')
+/*
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+//console.log(gameName.__proto__);
+console.log(gameName.toLowerCase());
+//console.log(gameName.indexOf('q', 1));
+*/
+/*
+console.log(gameName)
+console.log(gameName.trim())
+*/
+/*
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'))
+console.log(url.includes('.com'))
+
+*/
+/*
+const gameName = new String('        hitesh-hc-com         ')
+const yo = gameName.trim();
+console.log(yo.split('-'))
+*/
+let yo = 5 ; 
+yo = 4 ;
+console.log(yo)
